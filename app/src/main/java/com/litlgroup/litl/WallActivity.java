c30b744951f62368b0d5db0856f19e606a0e5326
Andrews, John L <John.L.Andrews@disney.com>
2016-08-17 12:24:16 -0700

    add TaskPager Adapter inside Wall Activity

--- app/src/main/java/com/litlgroup/litl/WallActivity.java
+++ app/src/main/java/com/litlgroup/litl/WallActivity.java
@@ -1,5 +1,8 @@
 package com.litlgroup.litl;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,33 @@ public class WallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall);
+    }
+
+
+    public class TaskPagerAdapter extends FragmentPagerAdapter {
+        private String tabTitles[] = {"Proposals", "Offers" };
+
+        public TaskPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new ProposalsFragment();
+            } else {
+                return new OffersFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
     }
 }
---
+++ app/src/main/java/com/litlgroup/litl/fragment/TaskFragment.java
@@ -0,0 +1,32 @@
+package com.litlgroup.litl.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.litlgroup.litl.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskFragment extends Fragment {
+
+
+    public TaskFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
