304062efbb0f15ce8d617d15e1339f3cf93e6595
Andrews, John L <John.L.Andrews@disney.com>
2016-08-20 15:07:37 -0700

    add recycle view adapter andfragment layout

---
+++ app/src/main/java/com/litlgroup/litl/adapter/TaskRecycleAdapter.java
@@ -0,0 +1,29 @@
+package com.litlgroup.litl.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by andrj148 on 8/20/16.
+ */
+public class TaskRecycleAdapter extends RecyclerView.Adapter<TaskRecycleAdapter.ViewHolder> {
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        }
+
+    @Override
+    public TaskRecycleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/litlgroup/litl/fragment/TaskFragment.java
+++ app/src/main/java/com/litlgroup/litl/fragment/TaskFragment.java
@@ -1,18 +1,41 @@
 package com.litlgroup.litl.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.litlgroup.litl.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TaskFragment extends Fragment {
 
+    private RecyclerView rvTasks;
+    private LinearLayoutManager linearLayoutManager;
 
     public TaskFragment() {
         // Required empty public constructor
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        setUpRecycleView(view);
+        return view;
+    }
 
+    private void setUpRecycleView(View v) {
+        rvTasks= (RecyclerView) v.findViewById(R.id.taskRecycleView);
+        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        rvTasks.setLayoutManager(linearLayoutManager);
 
+    }
 }
