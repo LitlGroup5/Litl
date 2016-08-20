17d4312f5fd29e96f9dd397493736fcdab318b0c
Andrews, John L <John.L.Andrews@disney.com>
2016-08-20 15:13:56 -0700

    create view holders

--- app/src/main/java/com/litlgroup/litl/adapter/TaskRecycleAdapter.java
+++ app/src/main/java/com/litlgroup/litl/adapter/TaskRecycleAdapter.java
@@ -1,7 +1,11 @@
 package com.litlgroup.litl.adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+
+import com.litlgroup.litl.R;
 
 /**
  * Created by andrj148 on 8/20/16.
@@ -9,12 +13,18 @@ import android.view.ViewGroup;
 public class TaskRecycleAdapter extends RecyclerView.Adapter<TaskRecycleAdapter.ViewHolder> {
 
         public static class ViewHolder extends RecyclerView.ViewHolder {
+            private CardView cardView;
 
+            public ViewHolder(CardView v) {
+                super(v);
+                cardView = v;
+            }
         }
 
     @Override
     public TaskRecycleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_task, parent, false);
+        return new ViewHolder(cv);
     }
 
     @Override
