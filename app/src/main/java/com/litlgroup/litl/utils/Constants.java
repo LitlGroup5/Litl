0abc063c794a1114d4289f836dae3ea821eb4414
Andrews, John L <John.L.Andrews@disney.com>
2016-08-23 16:59:10 -0700

    fix constants

--- app/src/main/java/com/litlgroup/litl/utils/Constants.java
+++ app/src/main/java/com/litlgroup/litl/utils/Constants.java
@@ -5,7 +5,7 @@ package com.litlgroup.litl.utils;
  */
 public class Constants {
 
-    public static String TASK_ID = "taskID";
-    public static String SELECTED_TASK = "selectedTask";
-    public static String TASK_TYPE = "taskType";
+    public static final String TASK_ID = "TaskID";
+    public static final String SELECTED_TASK = "SelectedTask";
+    public static final String TASK_TYPE = "TaskType";
 }
