faab7636103f128fc87326bb2b7be4b40be6ee87
iamjohnandrews <john@soothsayerlabs.com>
2016-09-02 12:00:09 -0700

    show no photo is no tasks in a category

--- app/src/main/java/com/litlgroup/litl/fragments/TaskFragment.java
+++ app/src/main/java/com/litlgroup/litl/fragments/TaskFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.litlgroup.litl.R;
 import com.litlgroup.litl.activities.TaskDetailActivity;
@@ -55,6 +56,7 @@ public class TaskFragment extends Fragment {
             setupSwipeToRefresh(view);
         } else {
             view = inflater.inflate(R.layout.fragment_no_tasks, container, false);
+            Toast.makeText(getActivity(), "No Tasks in this category. Please create one", Toast.LENGTH_SHORT).show();
         }
 
         return view;
